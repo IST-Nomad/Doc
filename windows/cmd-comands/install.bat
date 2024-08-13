b7489dc55c62fca4236f2e8746485d04c99b3cb7
Batr <poplavkovsemen@gmail.com>
2024-08-13 16:26:40 +0800

    13.08

--- windows/cmd-comands/install.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-@call \\10.129.135.200\netlogon\Scripts\term.bat pause | @call \\10.129.135.200\netlogon\Scripts\term_test.bat pause | @call \\10.129.135.200\netlogon\Scripts\miranda.bat pause | @call \\10.129.135.200\netlogon\Scripts\link.bat pause | @call \\10.129.135.200\netlogon\Scripts\link_rst.bat pause | @call \\10.129.135.200\netlogon\Scripts\link_m.bat
