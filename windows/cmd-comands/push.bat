da34361c7f70fcebb58fd8bb547dbfb1fb2bc66f
Batr <poplavkovsemen@gmail.com>
2024-08-22 18:00:30 +0800

    08.22.18:00

--- windows/cmd-comands/push.bat
+++ windows/cmd-comands/push.bat
@@ -5,11 +5,10 @@ Set f_Time=%Time%
 Set f_Day=%f_Date:~0,2%
 Set f_Month=%f_Date:~3,2%
 Set f_Year=%f_Date:~6,4%
-Set f_Hour=%Time:~0,4%
+Set f_Hour=%Time:~0,5%
 
 Set f_Month=%f_Month: =%
 Set f_Day=%f_Day: =%
-Set f_Hour=%f_Time: =%
 
 cd C:\Users\oit_10\Documents\Doc
 git add . | git commit -m "%f_Month%.%f_Day%.%f_Hour%" | git push origin work
