ed8993d4b22d350c737b05ff1b35e22ee7e79f38
Batr <poplavkovsemen@gmail.com>
2024-09-23 18:00:31 +0800

    18:00 23.09

---
+++ windows/cmd-comands/notepad_add.bat
@@ -0,0 +1,7 @@
+@echo off
+SET nppPath=C:\Program Files (x86)\Notepad++\notepad++.exe
+SET itemName=Edit with Notepad++
+rem all file types
+reg add "HKEY_CLASSES_ROOT\*\shell\%itemName%"         /t REG_SZ /v "" /d "%itemName%"   /f
+reg add "HKEY_CLASSES_ROOT\*\shell\%itemName%"         /t REG_EXPAND_SZ /v "Icon" /d "%nppPath%,0" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\%itemName%\command" /t REG_SZ /v "" /d "%nppPath% \"%%1\"" /f
