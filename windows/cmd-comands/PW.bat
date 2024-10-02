e80cbe14e3fe1aa2259d29ef1f3e17c17418ead4
Batr <istnomad@yandex.ru>
2024-10-02 21:44:21 +0800

    Home_10.02.21:44

--- windows/cmd-comands/PW.bat
+++
@@ -1,5 +0,0 @@
-echo
-rem выбираем кодировку для русского языка
-chcp 65001
-rem копируем 
-robocopy C:\Users\oit_10\Documents\Doc\ "T:\600 - Отдел Информационных Технологий\BVV\Doc" /MIR /XF file .gitignore /XD dirs .git
