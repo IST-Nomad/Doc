052a7af6c0670d5de514ce5da5d02799238aef29
Batr <poplavkovsemen@gmail.com>
2024-10-02 21:46:10 +0800

    Work_21:46 02.10

--- windows/cmd-comands/finist_gestbank.bat
+++
@@ -1,8 +0,0 @@
-echo off
-RMDIR \\GESTBANK\c$\Finist\Client /S /Q 
-TIMEOUT 5
-RMDIR \\GESTBANK\c$\Finist\WFinInst /S /Q 
-TIMEOUT 5
-robocopy \\FINIST\c$\Finist\GEstBank\Client \\GESTBANK\c$\Finist\Client /E /R:1 /W:0
-TIMEOUT 5
-robocopy \\FINIST\c$\Finist\WFinInst \\GESTBANK\c$\Finist\WFinInst /E /R:1 /W:0
