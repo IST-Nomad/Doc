fde424c63dfaa171f06b3f6963dbc210bf35cd8e
Batr <istnomad@yandex.ru>
2024-10-08 19:18:27 +0800

    Home_10.08.19:18

---
+++ windows/cmd-comands/pull.bat
@@ -0,0 +1,14 @@
+echo off
+
+Set f_Date=%Date%
+Set f_Time=%Time%
+Set f_Day=%f_Date:~0,2%
+Set f_Month=%f_Date:~3,2%
+Set f_Year=%f_Date:~6,4%
+Set f_Hour=%Time:~0,5%
+
+Set f_Month=%f_Month: =%
+Set f_Day=%f_Day: =%
+
+cd C:\Users\batr\Documents\Doc
+git pull origin work
---
+++ windows/cmd-comands/synchronization.bat
@@ -0,0 +1,7 @@
+echo off
+rem выбираем кодировку для русского языка
+chcp 65001
+rem копируем 
+robocopy F:\book D:\book *.pdf *.epub /E /Z /R:3 /W:1
+robocopy F:\Soft D:\Soft /E /Z /R:3 /W:1
+robocopy C:\Users\batr\Downloads D:\book *.pdf *.epub /MOVE /E /Z /R:3 /W:1
